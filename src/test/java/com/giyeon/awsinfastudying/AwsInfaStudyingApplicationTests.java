1d3ddb26c7f12ee77b3b4c7b6eb75e155dade89c
GiyeonChoi <bdcgy22@daum.net>
2025-01-22 21:35:55 +0900

    test디렉토리 ignore

--- src/test/java/com/giyeon/awsinfastudying/AwsInfaStudyingApplicationTests.java
+++ src/test/java/com/giyeon/awsinfastudying/AwsInfaStudyingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.giyeon.awsinfastudying;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AwsInfaStudyingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.giyeon.awsinfastudying;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AwsInfaStudyingApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
