e31bafe05b0df8b57e9de66c4a124bf19ea73c58
GiyeonChoi <bdcgy22@daum.net>
2025-01-23 11:59:58 +0900

    도메인 구입 후 연결테스트

--- src/main/java/com/giyeon/awsinfastudying/controller/TestController.java
+++ src/main/java/com/giyeon/awsinfastudying/controller/TestController.java
@@ -2,15 +2,16 @@ package com.giyeon.awsinfastudying.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<?> test() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("Hello from /api/test");
     }
-
 }
